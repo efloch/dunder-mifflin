d94cf354bd3e649865ff6789e690d73c1fe4762b
Clement <clemper11@gmail.com>
2020-05-02 18:47:03 +0200

    adding list of names and mentions extractions

--- src/params.py
+++ src/params.py
@@ -1,4 +1,13 @@
 from src.config import *
 
 COUNT_METHOD = ['words', 'lines']
+
 STOPWORDS = set(stopwords.words('english'))
+
+# Characters with at least 100 lines
+NAMES_LIST = ['Michael', 'Dwight', 'Jim', 'Pam', 'Andy',
+              'Kevin', 'Ryan', 'Angela', 'Oscar', 'Erin',
+              'Phyllis', 'Jan', 'Kelly', 'Darryl', 'Toby',
+              'Stanley', 'Holly', 'Meredith', 'Creed', 'Gabe',
+              'David', 'Karen', 'Robert', 'Roy', 'Nellie',
+              'Charles', 'Jo', 'Deangelo']
--- src/process_lines.py
+++ src/process_lines.py
@@ -48,37 +48,19 @@ def remove_stopwords(tokens, stop_words=STOPWORDS):
     return filtered_tokens
 
 
-def get_sentiment_score(line):
-    """
-
-    Args:
-        line (str):
-
-    Returns:
-        Sentiment score between -1 and 1 using textblob sentiment lexicon
-    """
-    score = textblob.TextBlob(line).sentiment.polarity
-    score = round(float(score), 2)
-    return score
-
-
 def process_lines(input_df):
     """
     Takes in Dataframe script and process "line" column which
     contains the text of each lines
     - Clean the text (column clean_line)
     - Tokenize it (tokenized_line)
-    - Add word count (word_count)
     - Get non stop words (tokenized_non_stop)
-    - Add sentiment score (sentiment_score)
 
     Args:
         input_df (pd.DataFrame): raw dataset containing one line per row
     """
     input_df['clean_line'] = input_df['line_text'].apply(clean_line)
     input_df['tokenized_line'] = input_df['clean_line'].apply(tokenize_line)
-    input_df['word_count'] = input_df['tokenized_line'].apply(len)
     input_df['tokenized_non_stop'] = input_df['tokenized_line'].apply(
         remove_stopwords)
-    input_df['sentiment_score'] = input_df['clean_line'].apply(get_sentiment_score)
     return input_df
